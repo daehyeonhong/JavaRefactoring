3d9b5adb6da91e1f71dd844aae7b2eceae885515
DaeHyeonHong <fholng@outlook.com>
2022-04-06 23:39:28 +0900

    [리팩토링]중복코드제거 함수 추출 코드 정리(슬라이드) 메소드 풀업

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
@@ -1,14 +1,34 @@
 package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Dashboard {
 
     public static void main(String[] args) throws IOException {
         ReviewerDashboard reviewerDashboard = new ReviewerDashboard();
-        reviewerDashboard.printReviewers();
-
+        reviewerDashboard.printReviewers(30);
         ParticipantDashboard participantDashboard = new ParticipantDashboard();
-        participantDashboard.printParticipants(15);
+        participantDashboard.printReviewers(15);
+    }
+
+    protected void printUsernames(final int eventId) throws IOException {
+        final GitHub gitHub = GitHub.connect();
+        final GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        final GHIssue issue = repository.getIssue(eventId);
+
+        // Get usernames
+        final Set<String> usernames = new HashSet<>();
+        issue.getComments()
+                .stream().map(GHIssueComment::getUserName)
+                .forEach(usernames::add);
+
+        // Print usernames
+        usernames.forEach(System.out::println);
     }
 }
