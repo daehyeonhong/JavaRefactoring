2b7ba5aed0d79a68d924bdc6604c81ba9fb2d41e
HonGDae <fholng@outlook.com>
2022-04-09 18:43:36 +0900

    긴 함수 -> 함수 명령으로 변경

--- src/main/java/me/whiteship/refactoring/_03_long_function/_07_replace_temp_with_query/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_07_replace_temp_with_query/StudyDashboard.java
@@ -4,10 +4,7 @@ import org.kohsuke.github.GHIssue;
 import org.kohsuke.github.GHIssueComment;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
@@ -15,9 +12,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class StudyDashboard {
+    private final int totalNumberOfEvents;
+
+    public StudyDashboard(final int totalNumberOfEvents) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        StudyDashboard studyDashboard = new StudyDashboard();
+        StudyDashboard studyDashboard = new StudyDashboard(15);
         studyDashboard.print();
     }
 
@@ -32,31 +34,18 @@ public class StudyDashboard {
 
         for (int index = 1; index <= totalNumberOfEvents; index++) {
             int eventId = index;
-            service.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        GHIssue issue = repository.getIssue(eventId);
-                        List<GHIssueComment> comments = issue.getComments();
+            service.execute(() -> {
+                try {
+                    GHIssue issue = repository.getIssue(eventId);
+                    List<GHIssueComment> comments = issue.getComments();
 
-                        for (GHIssueComment comment : comments) {
-                            String username = comment.getUserName();
-                            boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
-                            Participant participant = null;
-                            if (isNewUser) {
-                                participant = new Participant(username);
-                                participants.add(participant);
-                            } else {
-                                participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-                            }
-
-                            participant.setHomeworkDone(eventId);
-                        }
-
-                        latch.countDown();
-                    } catch (IOException e) {
-                        throw new IllegalArgumentException(e);
+                    for (final GHIssueComment comment : comments) {
+                        Participant participant = this.findParticipant(comment.getUserName(), participants);
+                        participant.setHomeworkDone(eventId);
                     }
+                    latch.countDown();
+                } catch (IOException e) {
+                    throw new IllegalArgumentException(e);
                 }
             });
         }
@@ -64,61 +53,19 @@ public class StudyDashboard {
         latch.await();
         service.shutdown();
 
-        try (FileWriter fileWriter = new FileWriter("participants.md");
-             PrintWriter writer = new PrintWriter(fileWriter)) {
-            participants.sort(Comparator.comparing(Participant::username));
+        new StudyPrinter(this.totalNumberOfEvents, participants).execute();
+    }
 
-            writer.print(header(totalNumberOfEvents, participants.size()));
-
-            participants.forEach(p -> {
-                String markdownForHomework = getMarkdownForParticipant(totalNumberOfEvents, p);
-                writer.print(markdownForHomework);
-            });
+    private Participant findParticipant(final String username, final List<Participant> participants) {
+        boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
+        Participant participant;
+        if (isNewUser) {
+            participant = new Participant(username);
+            participants.add(participant);
+        } else {
+            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
         }
+        return participant;
     }
 
-    private double getRate(final int totalNumberOfEvents, final Participant p) {
-        long count = p.homework().values().stream()
-                .filter(v -> v)
-                .count();
-        return (double) (count * 100 / totalNumberOfEvents);
-    }
-
-    private String getMarkdownForParticipant(final int totalNumberOfEvents, final Participant p) {
-        double rate = getRate(totalNumberOfEvents, p);
-        return String.format("| %s %s | %.2f%% |\n", p.username(), checkMark(p, totalNumberOfEvents), rate);
-    }
-
-    /**
-     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
-     * | --- | --- | --- | --- | --- |
-     */
-    private String header(int totalEvents, int totalNumberOfParticipants) {
-        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
-
-        for (int index = 1; index <= totalEvents; index++) {
-            header.append(String.format(" %d주차 |", index));
-        }
-        header.append(" 참석율 |\n");
-
-        header.append("| --- ".repeat(Math.max(0, totalEvents + 2)));
-        header.append("|\n");
-
-        return header.toString();
-    }
-
-    /**
-     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
-     */
-    private String checkMark(Participant p, int totalEvents) {
-        StringBuilder line = new StringBuilder();
-        for (int i = 1; i <= totalEvents; i++) {
-            if (p.homework().containsKey(i) && p.homework().get(i)) {
-                line.append("|:white_check_mark:");
-            } else {
-                line.append("|:x:");
-            }
-        }
-        return line.toString();
-    }
 }
