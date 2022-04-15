53abe39d126a1265a0b6b1a453337ef2662451f6
DaeHyeonHong <fholng@outlook.com>
2022-04-15 20:48:51 +0900

    [리팩토링] 전역데이터 변수 캡슐화

--- src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
+++ src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
@@ -1,13 +1,45 @@
 package me.whiteship.refactoring._05_global_data._17_encapsulate_variable;
 
 public class Thermostats {
+    private static Integer targetTemperature = 70;
+    private static Boolean heating = true;
+    private static Boolean cooling = false;
+    private static Boolean readInFahrenheit = true;
 
-    public static Integer targetTemperature = 70;
+    protected Thermostats() {
+    }
 
-    public static Boolean heating = true;
+    public static Integer getTargetTemperature() {
+        return targetTemperature;
+    }
 
-    public static Boolean cooling = false;
+    public static void setTargetTemperature(final Integer targetTemperature) {
+//        TODO validation
+        Thermostats.targetTemperature = targetTemperature;
+//        TODO notify
+    }
 
-    public static Boolean readInFahrenheit = true;
+    public static Boolean getHeating() {
+        return heating;
+    }
 
+    public static void setHeating(final Boolean heating) {
+        Thermostats.heating = heating;
+    }
+
+    public static Boolean getCooling() {
+        return cooling;
+    }
+
+    public static void setCooling(final Boolean cooling) {
+        Thermostats.cooling = cooling;
+    }
+
+    public static Boolean getReadInFahrenheit() {
+        return readInFahrenheit;
+    }
+
+    public static void setReadInFahrenheit(final Boolean readInFahrenheit) {
+        Thermostats.readInFahrenheit = readInFahrenheit;
+    }
 }
