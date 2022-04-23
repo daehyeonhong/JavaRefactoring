c0af808717a180447606d794b2b842bdba228803
DaeHyeonHong <fholng@outlook.com>
2022-04-23 21:46:26 +0900

    [리팩토링] 가변 데이터 reference => value

---
+++ src/test/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumberTest.java
@@ -0,0 +1,15 @@
+package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TelephoneNumberTest {
+
+    @Test
+    void testEquals() {
+        final TelephoneNumber number1 = new TelephoneNumber("123", "1234");
+        final TelephoneNumber number2 = new TelephoneNumber("123", "1234");
+        assertEquals(number1, number2);
+    }
+
+}
