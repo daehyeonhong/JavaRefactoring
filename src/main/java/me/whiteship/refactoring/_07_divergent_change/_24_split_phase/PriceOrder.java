98f3423cc9a9afd0e9d76142a91fbe3319a7169e
DaeHyeonHong <fholng@outlook.com>
2022-04-24 22:18:33 +0900

    [리팩토링] 뒤엉킨 변경 단계 쪼개기

--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,13 +3,19 @@ package me.whiteship.refactoring._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        final PriceData priceData = calculatePriceData(product, quantity);
+        return applyShipping(priceData, shippingMethod);
+    }
+
+    private PriceData calculatePriceData(final Product product, final int quantity) {
         final double basePrice = product.basePrice() * quantity;
-        final double discount = Math.max(quantity - product.discountThreshold(), 0)
-                * product.basePrice() * product.discountRate();
-        final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
-                shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        final double shippingCost = quantity * shippingPerCase;
-        final double price = basePrice - discount + shippingCost;
-        return price;
+        final double discount = Math.max(quantity - product.discountThreshold(), 0) * product.basePrice() * product.discountRate();
+        return new PriceData(basePrice, discount, quantity);
+    }
+
+    private double applyShipping(final PriceData priceData, final ShippingMethod shippingMethod) {
+        final double shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold()) ? shippingMethod.discountedFee() : shippingMethod.feePerCase();
+        final double shippingCost = priceData.quantity() * shippingPerCase;
+        return priceData.basePrice() - priceData.discount() + shippingCost;
     }
 }
