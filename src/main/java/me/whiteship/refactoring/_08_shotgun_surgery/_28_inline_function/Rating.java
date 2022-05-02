650c8b1e6c939546124d584abbafa260c8bc2e42
hongdae <fholng@outlook.com>
2022-05-02 22:48:11 +0900

    inlineFunctions

--- src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_28_inline_function/Rating.java
+++ src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_28_inline_function/Rating.java
@@ -1,14 +1,9 @@
 package me.whiteship.refactoring._08_shotgun_surgery._28_inline_function;
 
-import java.sql.Driver;
-
 public class Rating {
 
-    public int rating(Driver driver) {
-        return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+    public int rating(final Driver driver) {
+        return driver.getNumberOfLateDeliveries() > 5 ? 2 : 1;
     }
 
-    private boolean moreThanFiveLateDeliveries(Driver driver) {
-        return driver.getNumberOfLateDeliveries() > 5;
-    }
 }
--- src/test/java/me/whiteship/refactoring/_08_shotgun_surgery/_28_inline_function/RatingTest.java
+++ src/test/java/me/whiteship/refactoring/_08_shotgun_surgery/_28_inline_function/RatingTest.java
@@ -1,14 +1,13 @@
 package me.whiteship.refactoring._08_shotgun_surgery._28_inline_function;
 
 import org.junit.jupiter.api.Test;
-import java.sql.Driver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RatingTest {
 
     @Test
     void rating() {
-        Rating rating = new Rating();
+        Rating rating= new Rating();
         assertEquals(2, rating.rating(new Driver(15)));
         assertEquals(1, rating.rating(new Driver(3)));
     }
