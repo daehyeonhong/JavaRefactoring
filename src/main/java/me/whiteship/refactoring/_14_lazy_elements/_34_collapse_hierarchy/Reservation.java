5780fd8cb0ec272d2e92f6f88b5fd58826196646
hongdae <fholng@outlook.com>
2022-05-08 00:04:23 +0900

    collapse hierarchy

--- src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/Reservation.java
+++ src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/Reservation.java
@@ -3,16 +3,6 @@ package me.whiteship.refactoring._14_lazy_elements._34_collapse_hierarchy;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class Reservation {
-
-    private LocalDateTime startDateTime;
-
-    private LocalDateTime endDateTime;
-
-    private List<String> members;
-
-    private String owner;
-
-    private boolean paid;
-
+public record Reservation(LocalDateTime endDateTime, List<String> members, String owner, boolean paid,
+                          LocalDateTime startDateTime, String courtNumber) {
 }
