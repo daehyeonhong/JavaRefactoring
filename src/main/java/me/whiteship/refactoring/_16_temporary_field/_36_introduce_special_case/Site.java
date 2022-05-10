b2b9ec6a78b3cbb4c3194b7afc62cf98fdcda6ca
daehyeonhong <fholng@outlook.com>
2022-05-09 21:02:43 -0700

    introduce special case

--- src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/Site.java
+++ src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/Site.java
@@ -1,14 +1,7 @@
 package me.whiteship.refactoring._16_temporary_field._36_introduce_special_case;
 
-public class Site {
-
-    private Customer customer;
-
+public record Site(Customer customer) {
     public Site(Customer customer) {
-        this.customer = customer;
-    }
-
-    public Customer getCustomer() {
-        return customer;
+        this.customer = customer.getName().equals("unknown") ? new UnknownCustomer() : customer;
     }
 }
