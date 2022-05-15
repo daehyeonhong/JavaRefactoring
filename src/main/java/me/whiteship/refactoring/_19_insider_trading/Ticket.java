8010c466089581a57f78f07a550704e7d8f40eaf
daehyeonhong <fholng@outlook.com>
2022-05-15 23:57:07 +0900

    insider trading

--- src/main/java/me/whiteship/refactoring/_19_insider_trading/Ticket.java
+++ src/main/java/me/whiteship/refactoring/_19_insider_trading/Ticket.java
@@ -2,22 +2,9 @@ package me.whiteship.refactoring._19_insider_trading;
 
 import java.time.LocalDate;
 
-public class Ticket {
-
-    private LocalDate purchasedDate;
-
-    private boolean prime;
-
-    public Ticket(LocalDate purchasedDate, boolean prime) {
-        this.purchasedDate = purchasedDate;
-        this.prime = prime;
-    }
-
-    public LocalDate getPurchasedDate() {
-        return purchasedDate;
-    }
-
-    public boolean isPrime() {
-        return prime;
+public record Ticket(LocalDate purchasedDate, boolean prime) {
+    public boolean isFastPass() {
+        LocalDate earlyBirdDate = LocalDate.of(2022, 1, 1);
+        return this.prime() && this.purchasedDate().isBefore(earlyBirdDate);
     }
 }
--- src/test/java/me/whiteship/refactoring/_19_insider_trading/CheckInTest.java
+++ src/test/java/me/whiteship/refactoring/_19_insider_trading/CheckInTest.java
@@ -9,10 +9,10 @@ class CheckInTest {
 
     @Test
     void isFastPass() {
-        CheckIn checkIn = new CheckIn();
-        assertTrue(checkIn.isFastPass(new Ticket(LocalDate.of(2021, 12, 31), true)));
-        assertFalse(checkIn.isFastPass(new Ticket(LocalDate.of(2021, 12, 31), false)));
-        assertFalse(checkIn.isFastPass(new Ticket(LocalDate.of(2022, 1, 2), true)));
+        final Ticket ticket = new Ticket(LocalDate.of(2021, 12, 31), true);
+        assertTrue(ticket.isFastPass());
+        assertFalse(new Ticket(LocalDate.of(2021, 12, 31), false).isFastPass());
+        assertFalse(new Ticket(LocalDate.of(2022, 1, 2), true).isFastPass());
     }
 
 }
