1de5f35306c11b4e3c38cfb6d1e911ec401acc68
daehyeonhong <fholng@outlook.com>
2022-05-16 21:53:25 +0900

    extract superclass

--- src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
+++ src/main/java/me/whiteship/refactoring/_20_large_class/_41_extract_superclass/Employee.java
@@ -1,26 +1,20 @@
 package me.whiteship.refactoring._20_large_class._41_extract_superclass;
 
-public class Employee {
-
+public class Employee extends Party {
     private Integer id;
-
-    private String name;
 
     private double monthlyCost;
 
-    public double annualCost() {
-        return this.monthlyCost * 12;
+    public Employee(final String name) {
+        super(name);
     }
 
     public Integer getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getMonthlyCost() {
+    @Override
+    public double monthlyCost() {
         return monthlyCost;
     }
 }
